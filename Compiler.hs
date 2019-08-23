a0725ae7dcefcc045b6d838a57f177b808f0973b
matheusantonio <matheus.antonio.c@outlook.com>
2019-08-23 12:34:33 -0300

    Adicionada funcionalidade para ler o nome do programa da linha de comando.

--- Compiler.hs
+++ Compiler.hs
@@ -1,5 +1,11 @@
 import System.IO
+import System.Environment
 
-main =
-    readFile "program" >>= -- substituir a string pelo caminho do programa
-    putStr -- substituir aqui pela chamada da função que irá separar os tokens
+-- Para executar, chame:
+-- runghc Compiler.hs "[nome do arquivo do programa]"
+-- a variável inf armazena uma string contendo o programa inteiro
+
+main = do
+    args <- getArgs
+    inf <- readFile (head args)
+    putStr inf -- substituir aqui pela chamada da função que irá separar os tokens
